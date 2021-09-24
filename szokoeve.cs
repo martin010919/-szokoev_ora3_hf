3cb4279acfa22fe30c42d53e715064dee9dfcca2
Mattgwat <70171040+martin010919@users.noreply.github.com>
2021-09-24 15:39:22 +0200

    3. heti hf, OE-AMK 2021 Foszk

---
+++ szokoeve.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace szökőéve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem az évszámot");
+            string evszam = Console.ReadLine();
+            int a = Convert.ToInt32(evszam);
+            DateTime.IsLeapYear(a);
+            
+            if (true a / 4.00);
+            {
+                Console.WriteLine("Szökőév!");
+            }
+            if (false a / 4.00);
+            {
+                Console.WriteLine("Nem szökőév");
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
